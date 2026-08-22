a157d39182f33d6f3ad18851f7089cd05049286b
NilotpalDas008 <nd8851715@gmail.com>
2026-08-22 08:55:49 +0530

    change the server from starting using http to start using Express (app.listen())

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,9 @@
+
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const mongoUri = process.env.MONGODB_URI || process.env.MONGO_URI;
-    if (!mongoUri) {
-      throw new Error("MONGODB_URI or MONGO_URI is missing in environment variables.");
-    }
+    const mongoUri = process.env.MONGODB_URI || process.env.MONGO_URI ;
     const conn = await mongoose.connect(mongoUri);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
